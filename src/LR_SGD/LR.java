760f4e3ba4ce1809fe3eda59967124c8fda3983c
TorinYu <yutaoitper@gmail.com>
2014-03-24 21:27:06 -0400

    Hashtable -> HashMap improve runtime

--- src/LR_SGD/LR.java
+++ src/LR_SGD/LR.java
@@ -1,4 +1,4 @@
-package LR_SGD;
+
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
@@ -6,7 +6,10 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashSet;
-import java.util.Hashtable;
+import java.util.HashMap;
+import java.util.Set;
+
+import com.sun.corba.se.impl.encoding.OSFCodeSetRegistry.Entry;
 
 //Here, the first argument 10000 is the vocabulary size. 
 //Second argument 0.5 is the initial value of the learning rate.   λ
@@ -25,32 +28,34 @@ public class LR {
 		/**
 		 *  Train Part 
 		 */
-		int k = 0; // Keep the iteration time
+		//int k = 0; // Keep the iteration time
 		String[] labelType = {"nl", "el", "ru", "sl", "pl", "ca", "fr", "tr", "hu", "de", "hr", "es", "ga", "pt"}; 
-		Hashtable<String, Hashtable<Integer, Integer>> A = new Hashtable<String, Hashtable<Integer,Integer>>();
-		Hashtable<String, Hashtable<Integer, Double>> B = new Hashtable<String, Hashtable<Integer,Double>>();
+		HashMap<String, HashMap<Integer, Integer>> A = new HashMap<String, HashMap<Integer,Integer>>();
+		HashMap<String, HashMap<Integer, Double>> B = new HashMap<String, HashMap<Integer,Double>>();
 		HashSet<String> record = new HashSet<String>();
 		for (String a : labelType) {
 			record.add(a);
-			A.put(a, new Hashtable<Integer, Integer>());
-			B.put(a, new Hashtable<Integer, Double>());
+			A.put(a, new HashMap<Integer, Integer>());
+			B.put(a, new HashMap<Integer, Double>());
 		}
 
 		int vocab_size = Integer.valueOf(args[0]);
-		double lr = 0.5;
-		double u = 0.1;
-		int times = Integer.valueOf(args[3]);
+		double initial_lr = Double.parseDouble(args[1]);
+		double u = Double.parseDouble(args[2]);
+		u = u *2;
+		//double decrease = 0.5;
+		//int times = Integer.valueOf(args[3]);
 		long datasize = Long.parseLong(args[4]);
-
+		long num_line = 0;
 		//FileInputStream in = new FileInputStream(args[5]); 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String strLine;
-
-		for (int i = 0; i < times; i++) {
-			try { 
-				k++;
-				long num_line = 0;
+		double lr = initial_lr;
+		int k = 1;
+		//for (int i = 1; i <= times; i++) {
+			try { 				
 				while ((strLine = br.readLine()) != null) {
+					
 					HashSet<String> zeroLabel = new HashSet<String>(record);
 					String[] label_text = strLine.split("\t");
 					String[] labels = label_text[0].split(",");
@@ -68,8 +73,8 @@ public class LR {
 					 */
 					for (String label : labels) {
 						zeroLabel.remove(label);   // Delete the labels with value 1
-						Hashtable<Integer, Double> Btemp = B.get(label);
-						Hashtable<Integer, Integer> Atemp = A.get(label);
+						HashMap<Integer, Double> Btemp = B.get(label);
+						HashMap<Integer, Integer> Atemp = A.get(label);
 
 						for (int value : wordsValue) {
 							if (!Atemp.containsKey(value)) {
@@ -80,14 +85,15 @@ public class LR {
 								double updated_Bj = Btemp.get(value) * Math.pow((1-lr*u) , (k-Atemp.get(value)));
 								Btemp.put(value, updated_Bj);
 								//Atemp.put(value, k);        //TODO when to update K
-
 							}
 						}
+						
 						double temp = 0.0;
 						for (int value : wordsValue) {
 							temp += Btemp.get(value);
 						}
 						double P = 1 / (1 + Math.exp(-temp));
+						
 						for (int value : wordsValue) {
 							double updated_Bj = Btemp.get(value);
 							updated_Bj += lr*(1-P);
@@ -95,17 +101,17 @@ public class LR {
 							Atemp.put(value, k);
 						}
 
-						// Update Hashtable for each label 
-						B.put(label, Btemp);    
-						A.put(label, Atemp);
+						// Update HashMap for each label 
+						//B.put(label, Btemp);    
+						//A.put(label, Atemp);
 					}
 					/**
 					 * For labels with value 0
 					 */
-					String[] zerolebels = (String[]) zeroLabel.toArray();
-					for (String label : zerolebels) {
-						Hashtable<Integer, Double> Btemp = B.get(label);
-						Hashtable<Integer, Integer> Atemp = A.get(label);
+					Set<String> zerolabels = zeroLabel;
+					for (String label : zerolabels) {
+						HashMap<Integer, Double> Btemp = B.get(label);
+						HashMap<Integer, Integer> Atemp = A.get(label);
 
 						for (int value : wordsValue) {
 							if (!Atemp.containsKey(value)) {
@@ -131,44 +137,58 @@ public class LR {
 						}
 
 						// Update Hashtable for each label 
-						B.put(label, Btemp);    
-						A.put(label, Atemp);
+						//B.put(label, Btemp);    
+						//A.put(label, Atemp);
 					}
 
 					num_line ++;
 					// Next iteration 
+//					if (num_line == datasize) {
+//						k++;
+//						num_line = 0;
+//						lr = initial_lr/(k*k);
+//					}
 					if (num_line == datasize) {
 						break;
 					}
+					
 				}
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		}
+		//}
 		/**
 		 *  Update Bj in the last time
 		 */
+
 		for (String a : B.keySet()) {
-			Hashtable<Integer, Double> Btemp = new Hashtable<Integer, Double>();
-			for (int b : B.get(a).keySet()) {
+			HashMap<Integer, Double> Btemp = B.get(a);
+			HashMap<Integer, Integer> Atemp = A.get(a);
+			for (int b : Btemp.keySet()) {
 				double updated_Bj = Btemp.get(b);
 				updated_Bj *= Math.pow((1-lr*u), k-A.get(a).get(b));
 				Btemp.put(b, updated_Bj);
 			}
-			B.put(a, Btemp);
+			//B.put(a, Btemp);
 		}
+//		for (java.util.Map.Entry<String, HashMap<Integer, Double>> Btemp : B.entrySet()) {
+//			
+//		}
+//		for (java.util.Map.Entry<Integer, Double> b :  Btemp.entrySet()) {
+//			
+//		}
 
 
 		/**
 		 *  Debug Train Part 
 		 */
-		for (String a : B.keySet()) {
-			Hashtable<Integer, Double> Btemp = new Hashtable<Integer, Double>();
-			for (int b : B.get(a).keySet()) {
-				System.out.println("label :" + a +  "Integer :" + b + "value : " + Btemp.get(b));
-			}
-		}
+//		for (String a : B.keySet()) {
+//			Hashtable<Integer, Double> Btemp = B.get(a);
+//			for (int b : Btemp.keySet()) {
+//				System.out.println("label :" + a +  " Integer :" + b + " value : " + Btemp.get(b));
+//			}
+//		}
 
 
 		/**
@@ -179,7 +199,8 @@ public class LR {
 		String testLine;
 		
 		while ( (testLine = testBr.readLine()) != null) {
-			String[] testWords = testLine.split(" ");
+			String[] label_words = testLine.split("\t");
+			String[] testWords = label_words[1].split(" ");
 			
 			int[] wordsValue = new int[testWords.length];
 			for (int j = 0; j < testWords.length; j++) {
@@ -191,17 +212,19 @@ public class LR {
 			}
 			int printRange = 0;
 			for (String a : labelType) {
-				Hashtable<Integer, Double> testTemp = B.get(a);
+				HashMap<Integer, Double> testTemp = B.get(a);
 				double temp = 0.0;
 				for (int value : wordsValue) {
-					temp += testTemp.get(value);
+					if (testTemp.containsKey(value)) {   // TODO Deal with unexisted values 
+						temp += testTemp.get(value);
+					}
 				}
 				double P = 1 / (1 + Math.exp(-temp));
 				if (printRange == 13) {
-					System.out.print(a + "\t" + P + "\n");
+					System.out.print(a + "\t" + P + "\n");  
 				} else {
 					System.out.print(a + "\t" + P + ",");
-				}
+				} 
 				printRange ++;
 			}
 		}
